694fb515533bef2e1426e4b819d44e0f99da6b6b
darckbike <darckbike.95@hotmail.fr>
2016-05-17 22:00:13 +0200

    ajout requette test->OK

--- UNO/TablesVC.swift
+++ UNO/TablesVC.swift
@@ -27,6 +27,7 @@ class TablesVC: UIViewController ,UITableViewDataSource , UITableViewDelegate, U
         tableView.rowHeight = 60
         self.tableView.reloadData()
         
+        recupdonnees()
         
         NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(TablesVC.keyboardWillShow(_:)), name: UIKeyboardWillShowNotification, object: nil)
         NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(TablesVC.keyboardWillHide(_:)), name: UIKeyboardWillHideNotification, object: nil)
@@ -273,5 +274,37 @@ class TablesVC: UIViewController ,UITableViewDataSource , UITableViewDelegate, U
         self.view.endEditing(true)
     }
     
-}
+    func recupdonnees(){
+        
+        let url:NSURL = NSURL(string: "http://dev.asmock.com/api/carte")!
+        let session = NSURLSession.sharedSession()
+        
+        let request = NSMutableURLRequest(URL: url)
+        request.HTTPMethod = "GET"
+        request.cachePolicy = NSURLRequestCachePolicy.ReloadIgnoringCacheData
+        /*
+        let paramString = "data=Hello"
+        request.HTTPBody = paramString.dataUsingEncoding(NSUTF8StringEncoding)
+        */
+        let task = session.dataTaskWithRequest(request) {
+            (
+            let data, let response, let error) in
+            
+            guard let _:NSData = data, let _:NSURLResponse = response  where error == nil else {
+                print("error")
+                return
+            }
+            
+            let jsonData_msg:Dictionary<String, AnyObject> = (try! NSJSONSerialization.JSONObjectWithData(data!, options:NSJSONReadingOptions.MutableContainers )) as! Dictionary<String, AnyObject>
+            
+            print(jsonData_msg)
+            
+        }
+        
+        task.resume()
+    
+    }
+    
 
+    
+}
